cd3fc4f3ba7a36f0960bca173c12e9dc4f81506d
Matteo Marinucci <matteo@meanme.com>
2016-09-14 19:07:47 -0700

    inital setup

---
+++ js/questions.js
@@ -0,0 +1,27 @@
+// math, geography, history, coding, vocab, sports
+
+/**
+
+  {
+    type: 'math',
+    question: 'What are the first 4 digits of pie',
+    answers: [
+      '3.141',
+      '3.142',
+      '3.143',
+      '3.144',
+    ],
+    correct: 0
+  }
+*/
+
+
+
+// Add your questions here
+var questions = [
+
+
+
+
+
+]
