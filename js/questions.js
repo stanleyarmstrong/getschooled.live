de426e41879f3651f447f7b9e6e0eb270cb33f1e
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-09-14 19:37:55 -0700

    Math

--- js/questions.js
+++ js/questions.js
@@ -19,6 +19,64 @@
 
 // Add your questions here
 var questions = [
+   {
+     type: 'math',
+     question: 'Where are the first 3 digits of Pi?',
+     answers:[
+       '3.14',
+       '3.15',
+       '3.16',
+       '3.13'
+
+     ],
+     correct: 0
+
+   },
+   {
+     type: 'math',
+     question: 'What is the first derivative of 2x?',
+     answers:[
+       '3',
+       '2',
+       '4',
+       '5'
+     ],
+     correct:2
+   },
+   {
+     type: 'math',
+     question: 'What is the derivative of velocity?',
+     answers:{
+       'velocity squared',
+       'position',
+       'acceleration',
+       'jerk'
+     },
+     correct: 3
+     {
+       type:'math',
+       question: 'What is the Pythagorean theorem?',
+       answer:[
+         "'a' squared minus 'b' squared equals 'c' squared",
+         "'c' squared plus 'a' squared equals 'b' squared",
+         "'b' squared plus 'c' squared equals 'a' squared",
+         "'a' squared plus 'b' squared equals 'c' squared"
+       ] ,
+       correct: 4
+     },
+     {
+       type:'math',
+       question: "What is a derivative?",
+       answer:[
+         'Average Rate of Change',
+         'Slope',
+         'Slope of Tangent Line',
+         "Slope of 'x' squared"
+       ],
+       correct: 3
+
+     },
+   
 
 
 
